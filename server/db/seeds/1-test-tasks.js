31b036da387d34b098dfc6c9748932ac5d173d85
yind48 <dylanyin48@gmail.com>
2023-09-08 17:05:20 +1200

    Update seeds

--- server/db/seeds/1-test-tasks.js
+++ server/db/seeds/1-test-tasks.js
@@ -5,8 +5,8 @@
 export async function seed(knex) {
   await knex('tasks').del()
   await knex('tasks').insert([
-    { id: 1, tasks: 'Make app1', completed: false },
-    { id: 2, tasks: 'Make app2', completed: false },
-    { id: 3, tasks: 'Make app3', completed: true },
+    { id: 1, tasks: 'Make app1' },
+    { id: 2, tasks: 'Make app2' },
+    { id: 3, tasks: 'Make app3' },
   ])
 }
